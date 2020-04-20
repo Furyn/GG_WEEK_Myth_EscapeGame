9817adbdabb54cae64984c8289a4f781d2017c2b
Sébastien <sebastien.guarober@gmail.com>
2020-04-20 13:48:31 +0200

    Added highlight effect

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -7,6 +7,8 @@ public class PlayerInventory : MonoBehaviour
     public GameObject currentlyEquipped;
     Ray ray;
 
+    public Transform highlighted;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,12 @@ public class PlayerInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(highlighted != null)
+        {
+            highlighted.GetComponent<MeshRenderer>().material.color = Color.white;
+            highlighted = null;
+        }
+
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         Debug.DrawRay(ray.origin, ray.direction * 50, Color.red);
@@ -23,6 +31,10 @@ public class PlayerInventory : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, 50))
         {
+            hit.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            highlighted = hit.transform;
+
             if (Input.GetMouseButtonDown(0))
             {
                 if (hit.transform.gameObject.CompareTag("Lock"))
@@ -44,6 +56,11 @@ public class PlayerInventory : MonoBehaviour
                     PickUp(hit.transform.gameObject);
                 }
             }
+        }
+
+        if(highlighted == null)
+        {
+
         }
     }
 
