d71418a6fc7460cef1492ff4373cdcd660f67e36
Sébastien <sebastien.guarober@gmail.com>
2020-04-20 13:20:54 +0200

    Added Lock & Key system + Prefabs folder

---
+++ Assets/Scripts/Lock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    public bool isUnlocked;
+
+    public void OpenLock(GameObject currentItem)
+    {
+        if(currentItem.CompareTag("Key"))
+        {
+            Debug.Log("Opened the lock");
+            isUnlocked = !isUnlocked;
+            currentItem.SetActive(false);
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("I need a key to open this...");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public GameObject currentlyEquipped;
+    Ray ray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 50, Color.red);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 50))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.transform.gameObject.CompareTag("Lock"))
+                {
+                    if(currentlyEquipped != null)
+                    {
+                        hit.transform.GetComponent<Lock>().OpenLock(currentlyEquipped.transform.gameObject);
+                        currentlyEquipped = null;
+                    }
+
+                    else
+                    {
+                        Debug.Log("I need a key to open this...");
+                    }
+                }
+
+                if (hit.transform.gameObject.CompareTag("Key"))
+                {
+                    PickUp(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    void PickUp(GameObject objToPickUp)
+    {
+        currentlyEquipped = objToPickUp;
+        objToPickUp.SetActive(false);
+    }
+
+}
