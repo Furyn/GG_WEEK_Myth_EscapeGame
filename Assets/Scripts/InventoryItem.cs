151224c3de2e2b5598fb83007614058565cda177
Sébastien <sebastien.guarober@gmail.com>
2020-04-21 15:39:10 +0200

    Inventory + Position autour du joueur (Cycling des objets à debug)

---
+++ Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class InventoryItem
+{
+    //[HideInInspector]
+    public GameObject itemGO;
+    public Transform slotTransform;
+
+    public InventoryItem(GameObject _itemGO, Transform _slotTransform)
+    {
+        itemGO = _itemGO;
+        slotTransform = _slotTransform;
+    }
+}
