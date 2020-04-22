eba625d4e20638c94bae148cbdab4349ba00ac28
Sébastien <sebastien.guarober@gmail.com>
2020-04-22 16:13:22 +0200

    Added weight system to objects

---
+++ Assets/Scripts/PickableObjectStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObjectStats : MonoBehaviour
+{
+    [Header("More weight = Thrown further")]
+    [Range(4, 20)]
+    public float Weight = 4;
+}
