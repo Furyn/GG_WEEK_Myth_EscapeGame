bf983132ce6f40eb8ede95fba56a034a365c0ae6
Vincent <vincent_1999@hotmail.fr>
2020-04-24 10:20:28 +0200

    Update Scene Video and add the true vidéo

---
+++ Assets/Scripts/ChangeSceneEndVideo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class ChangeSceneEndVideo : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string nameOhTheSceneToLoad = "TRUC";
+    private bool isStarting = false;
+
+    private void Update()
+    {
+
+        if (videoPlayer.isPlaying)
+        {
+            isStarting = true;
+        }
+
+        if ( (!videoPlayer.isPlaying && isStarting) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(nameOhTheSceneToLoad));
+        }
+    }
+}
